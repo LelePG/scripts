260bf972a9a6ed9b0724e70246f7475588830d0c
Letícia Garcez <lpgarcez@protonmail.com>
2023-08-30 21:25:07 -0300

    renomeação do script de trabalho

--- trabalho.sh
+++ trabalho.sh
@@ -3,9 +3,11 @@
 # alias nome='CAMINHO' e depois disso source no arquivo que foi editado pro alias ficar disponível.
 
 LINKS=(
-
+   
 )
 
 for link in "${LINKS[@]}"; do
-    brave --new-tab "$link" 2>/dev/null # no momento estou usando o brave, mas se você estiver usando outro navegador, altere o navegador nesse comando.
+   nohup brave --new-tab "$link" &>/dev/null & # no momento estou usando o brave, mas se você estiver usando outro navegador, altere o navegador nesse comando.
 done
+
+nohup flatpak run com.spotify.Client &> /dev/null &
